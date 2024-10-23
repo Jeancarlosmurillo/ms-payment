366b98901048fe2a93ad925adecfa44ba4535113
Jeancarlosmurillo <jeanca1229@gmail.com>
2024-10-22 20:01:43 -0500

    conexion con notification

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from flask import Flask, request, jsonify
 import os
 import json
+import requests  # Asegúrate de tener importado requests para las notificaciones
 from dotenv import load_dotenv
 from epaycosdk.epayco import Epayco
-
 
 load_dotenv()
 
@@ -13,11 +13,11 @@ app = Flask(__name__)
 epayco = Epayco({
     'apiKey': os.getenv('PUBLIC_KEY'),
     'privateKey': os.getenv('PRIVATE_KEY'),
-        'lenguage': 'ES', #lenjuage de los mensajes
+    'lenguage': 'ES',  # lenjuage de los mensajes
     'test': os.getenv('EPAYCO_TEST') == 'true'
 })
 
-#metodo para crear un token de la tarjeta //
+# Método para crear un token de la tarjeta
 def create_token(data):
     try:
         card_info = {
@@ -25,13 +25,14 @@ def create_token(data):
             "card[exp_year]": data['exp_year'],
             "card[exp_month]": data['exp_month'],
             "card[cvc]": data['cvc'],
-            "hasCvv": True  # hasCvv: validar codigo de seguridad en la transacción
+            "hasCvv": True  # hasCvv: validar código de seguridad en la transacción
         }
         token = epayco.token.create(card_info)
         return token
     except Exception as e:
         return {'error': str(e)}
-#metodo para crear un cliente
+
+# Método para crear un cliente
 def create_customer(token, data):
     customer_info = {
         'name': data['name'],
@@ -39,7 +40,6 @@ def create_customer(token, data):
         'email': data['email'],
         'phone': data['phone'],
         'default': True
-
     }
     customer_info['token_card'] = token
 
@@ -49,7 +49,7 @@ def create_customer(token, data):
     except Exception as e:
         return {'error': str(e)}
 
-
+# Método para procesar el pago
 def process_payment(data, customer_id, token_card):
     try:
         payment_info = {
@@ -70,7 +70,6 @@ def process_payment(data, customer_id, token_card):
             'tax': '0',
             'tax_base': data['value'],
             'currency': 'COP'
-
         }
         print(f"Payment Info: {json.dumps(payment_info, indent=4)}")  # Imprime los datos de la solicitud
 
@@ -79,36 +78,57 @@ def process_payment(data, customer_id, token_card):
     except Exception as e:
         return {'error': str(e)}
 
-#enpoint para manejar todo el flujo de pago
+# Método para enviar notificación del pago
+def send_payment_notification(data):
+    try:
+        notification_data = {
+            "recipient": data['email'],  # Correo del usuario
+            "username": data['name'],  # Nombre del usuario
+            "amount": data['value'],  # Monto del pago
+            "description": 'Pago recibido con éxito'
+        }
+        response = requests.post('http://localhost:5000/paymentNotification', json=notification_data)
+        return response.json()
+    except Exception as e:
+        return {'error': str(e)}
+
+# Endpoint para manejar todo el flujo de pago
 @app.route('/process_payment', methods=['POST'])
 def handle_process_payment():
     data = request.json
 
-#crea el token de la tarjeta
+    # Crea el token de la tarjeta
     token_response = create_token(data)
     print("Token response", json.dumps(token_response))
 
-#verificar si hubo error al crear el token
+    # Verificar si hubo error al crear el token
     if token_response["status"] is False:
         return jsonify(token_response), 500
 
-    token_card = token_response['id'] #extrae el id del token
+    token_card = token_response['id']  # Extrae el id del token
 
-    #Crear cliente
+    # Crear cliente
     customer_response = create_customer(token_card, data)
     print("Customer response", json.dumps(customer_response))
 
-    #Verificar si hubo error al crear el cliente
+    # Verificar si hubo error al crear el cliente
     if 'error' in customer_response:
         return jsonify(customer_response), 500
 
     customer_id = customer_response['data']['customerId']
 
+    # Procesar el pago
     payment_response = process_payment(data, customer_id, token_card)
     print("Payment response", json.dumps(payment_response))
 
+    # Verificar si hubo error en el pago
     if 'error' in payment_response:
         return jsonify(payment_response), 500
+
+    # Enviar notificación del pago
+    notification_response = send_payment_notification(data)
+    print("Notification response", json.dumps(notification_response))
+
     return jsonify(payment_response), 200
 
 if __name__ == '__main__':
